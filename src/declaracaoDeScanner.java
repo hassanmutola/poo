27556e098754858095e51384f9416adf17afad6b
hassan mutola <hassanmx57@gmail.com>
2019-12-20 09:24:08 +0200

    construtores

--- src/declaracaoDeScanner.java
+++ src/declaracaoDeScanner.java
@@ -15,4 +15,29 @@ public class declaracaoDeScanner {
         }
         sc.close();
     }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
+    }
 }
