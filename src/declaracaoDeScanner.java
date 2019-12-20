2fe82bfd250ed915f76bfb01380b28b6a63e98cd
hassan mutola <hassanmx57@gmail.com>
2019-12-20 09:20:17 +0200

    programa concluido

--- src/declaracaoDeScanner.java
+++ src/declaracaoDeScanner.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 
 /**
  *
  * @author Hassan
  */
 public class declaracaoDeScanner {
-    
+    public static void main(String[] args) {
+        int i = 0;
+        Scanner sc = new Scanner (System.in);
+        System.out.print("como se chamas?");
+        while (sc.hasNext()){
+            i++;
+            System.out.println("Token: "+sc.next());
+        }
+        sc.close();
+    }
 }
