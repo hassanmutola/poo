1640f0edfe13e83b934cc2fd6113b6911cdb2c36
hassan mutola <hassanmx57@gmail.com>
2019-12-21 14:01:30 +0200

    trabalho gravado

--- src/poo/exercicio1.java
+++ src/poo/exercicio1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package poo;
 
 /**
@@ -10,5 +6,9 @@ package poo;
  * @author Hassan
  */
 public class exercicio1 {
+    public static void main(String[] args) {
+        double tempDia001 = 31.1;
+        
+    }
     
 }
