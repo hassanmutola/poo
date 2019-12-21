ae0a5c65744b83253671ca56377a4299f6d641f0
hassan mutola <hassanmx57@gmail.com>
2019-12-21 15:57:37 +0200

    excepcao

--- src/poo/exercicio3.java
+++ src/poo/exercicio3.java
@@ -10,5 +10,14 @@ public class exercicio3 {
          Scanner scan = new Scanner(System.in);
          
          System.out.println("entre com o numero de pessoas que serao entrevistadas");
+         int numEntrevistados = scan.nextint();
+         
+         String[][] nomesFilhos = new String[numEntrevistados][];
+         
+         for (int i=0; i<nomesFilhos.length; i++){
+         
+             System.out.println("entre com a quantidade de filhos");
+             
+         }
     }
 }
