25dd1c207a32fbd523b5ee8c7d7bbc6f9d93f08f
hassan mutola <hassanmx57@gmail.com>
2019-12-21 15:18:02 +0200

    dificil

--- src/poo/exercicio2.java
+++ src/poo/exercicio2.java
@@ -64,7 +64,7 @@ public class exercicio2 {
 
     @Override
     public int hashCode() {
-        return super.hashCode(); //To change body of generated methods, choose Tools | Tes.
+        return super.hashCode(); //To change body of generated methods, choose Tools | Ts.
     }
 
    
