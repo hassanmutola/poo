d2b8eef57748d3ac1f701110ad16ae7536e88eb3
hassan mutola <hassanmx57@gmail.com>
2019-12-21 16:06:21 +0200

    excepcao ainda

--- src/poo/exercicio3.java
+++ src/poo/exercicio3.java
@@ -7,16 +7,24 @@ package poo;
 public class exercicio3 {
     
     public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
+Scanner scan = new Scanner(System.in);
          
-         System.out.println("entre com o numero de pessoas que serao entrevistadas");
-         int numEntrevistados = scan.nextint();
+System.out.println("entre com o numero de pessoas que serao entrevistadas");
+int numEntrevistados = scan.nextint();
          
-         String[][] nomesFilhos = new String[numEntrevistados][];
+String[][] nomesFilhos = new String[numEntrevistados][];
          
-         for (int i=0; i<nomesFilhos.length; i++){
+for (int i=0; i<nomesFilhos.length; i++){
          
-             System.out.println("entre com a quantidade de filhos");
+System.out.println("entre com a quantidade de filhos");
+int qtdFilhos = scan.nextint();
+             
+nomesFilhos[i] = new String[qtdFilhos];
+             
+ for (int j=0; j<nomesFilhos[i][j].length(); j++){
+     
+     System.out.println("digite o nome do filho" + (j+1));                
+             }
              
          }
     }
