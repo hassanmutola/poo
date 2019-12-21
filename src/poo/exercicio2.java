fb9dc7e288098247c8de4a631a168e1187ced1d6
hassan mutola <hassanmx57@gmail.com>
2019-12-21 14:48:30 +0200

    deu erro

--- src/poo/exercicio2.java
+++ src/poo/exercicio2.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package poo;
 
 /**
@@ -10,5 +6,22 @@ package poo;
  * @author Hassan
  */
 public class exercicio2 {
-    
+    public static void main(String[] args) {
+        
+        double [][] notasAlunos = new double[30][4];
+        notasAlunos [0][1] = 10;
+        notasAlunos [0][2] = 7;
+        notasAlunos [0][3] = 9;
+        notasAlunos [0][4] = 9.5;
+        
+        notasAlunos [1][0] = 9;
+        notasAlunos [1][1] = 8;
+        notasAlunos [1][2] = 7;
+        notasAlunos [1][3] = 9;
+        
+        notasAlunos [2][0] = 8;
+        notasAlunos [2][1] = 9;
+        notasAlunos [2][2] = 10;
+        notasAlunos [2][3] = 7;
+    }
 }
