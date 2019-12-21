ce4944cb2e751e6e0f7db8ea0bf61e2b242e951f
hassan mutola <hassanmx57@gmail.com>
2019-12-21 15:43:21 +0200

    ele negou

--- src/poo/exercicio3.java
+++ src/poo/exercicio3.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poo;
 
 /**
@@ -11,4 +6,7 @@ package poo;
  */
 public class exercicio3 {
     
+    public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+    }
 }
